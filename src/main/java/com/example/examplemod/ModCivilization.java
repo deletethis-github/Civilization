58b8aa771a0408c9a1023f031a973a114a01e4f6
deletethis <mcdeletethis@gmail.com>
2015-10-07 14:51:13 -0700

    Set up workspace

---
+++ src/main/java/com/example/examplemod/ModCivilization.java
@@ -0,0 +1,15 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION)
+public class ModCivilization
+{
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
